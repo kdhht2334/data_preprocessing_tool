086afca6131090181fb0f02891cb4ae5b76ac671
seu5022 <kdhht5022@gmail.com>
2018-06-16 16:05:53 +0900

    README.md
    README.md

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # data_preprocessing_tool
 
-- Usage
+####- Usage
 ```
-python annotation.py --class example --start_no 10 --end_no 25
+git clone https://github.com/kdhht2334/data_preprocessing_tool
+cd data_preprocessing_tool
+python annotation.py --directory <directory> --start_no <start_no> --end_no <end_no>
 ```
 
-- Milestone
+* __start_no__ is start number of your pictures and __end_no__ is end number of your pictures
+* __end_no__ = __start_no__ + (number of all pictures in directory) - 1
+
+
+####- Milestone
 * [x] Add rename process
 * [x] Add format convert process
--- annotation.py
+++ annotation.py
@@ -8,7 +8,7 @@ import os
 import argparse
 
 app = argparse.ArgumentParser()
-app.add_argument("-n", "--class", type=str, default="box1",
+app.add_argument("-n", "--directory", type=str, default="box1",
                  help="Which categories want to select among 10 classes.")
 app.add_argument("-start", "--start_no", type=int, default=1,
                  help="Start number of .jpg files.")
@@ -16,15 +16,15 @@ app.add_argument("-end", "--end_no", type=int, default=10,
                  help="End number of .jpg files.")
 args = vars(app.parse_args())
 
-cls = args["class"]
+dr = args["directory"]
 start_no = args["start_no"]
 end_no = args["end_no"]
 
-category = str(cls)
+directory = str(dr)
 start_no = int(start_no)
 end_no = int(end_no)
 
-path = 'DB/' + str(category)
+path = str(directory)
 files = os.listdir(path)
 i = int(start_no)
 
@@ -50,12 +50,6 @@ for i in range(start_no,end_no+1,1):
     else:
         name = '00000' + str(i)
         
-    img1 = cv2.imread('DB/' + str(category) +'/' + str(name) + '.JPG')
+    img1 = cv2.imread(str(directory) +'/' + str(name) + '.JPG')
     img1 = cv2.resize(img1, (600, 400), interpolation=cv2.INTER_CUBIC)
-    cv2.imwrite('DB/' + str(category) +'/' + str(name) + '.JPG', img1)
-
-# convert JPG to jpg format
-cmd1 = ['mogrify -format jpg /your/path/example/* .JPG']
-subprocess.call(cmd1, shell=True)
-cmd2 = ['rm /your/path/example/*.JPG']
-subprocess.call(cmd2, shell=True)
+    cv2.imwrite(str(directory) +'/' + str(name) + '.jpg', img1)
